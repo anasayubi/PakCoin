a987e7c9cc38942f0bc1dbade7dadff530b779ae
anasayubi <anasayubi@hotmail.com>
2017-03-30 15:01:22 +0500

    reform coinbase

--- coinbase.js
+++ coinbase.js
@@ -1,8 +1,8 @@
 // This file initisalises coinbase for the application
 // Set api key and api secret key
-var apiKey = 'IkOPOzwEX3OzXZXv'
-var apiSecretKey = 'MjlKlGePwIXMG6mgI4vmOiZ7NLD6avLG'
-
+var apiKey = ''
+var apiSecretKey = ''
+ 
 // Coinbase client for accessing accounts/wallets
 var Client = require('coinbase').Client
 // Initialise coinbase client with registered account
