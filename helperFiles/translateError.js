963352db4c025873d68026701da8f4f659e8366e
anasayubi <anasayubi@hotmail.com>
2017-04-03 11:42:38 +0500

    add validation error translation
    add to translateError.ejs

--- helperFiles/translateError.js
+++ helperFiles/translateError.js
@@ -2,12 +2,19 @@
 // meaningful, user-oriented error messages
 function translateError(err){
     var message
-    if(err === ''){
-        message = ''
-    }
-    else{
+    //console.log('debug')
+    //console.log(err.errors)
+    if(Object.keys(err.errors).includes('firstName') && err.name === 'ValidationError')
+        message = 'First name can only letters, commas, periods, spaces and apostrophes'
+    else if(Object.keys(err.errors).includes('lastName') && err.name === 'ValidationError')
+        message = 'Last name can only letters, commas, periods, spaces and apostrophes'
+    else if(Object.keys(err.errors).includes('username') && err.name === 'ValidationError')
+        message = 'Username must at least be 5 characters and can contain letters, numbers, underscores, hyphens and periods'
+    else if(Object.keys(err.errors).includes('password') && err.name === 'ValidationError')
+        message = 'Password must at least 8 characters and must contain at least 1 number'
+    // Default message error
+    else
         message = "An error occured"
-    }
     
     return message
 }
